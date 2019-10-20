0cc017e8df2cc5b8906f25d0c28cac2cb16e00a4
Sam O'Keefe <sjo91190@gmail.com>
2019-10-19 19:24:40 -0600

    PEP Formatting

--- gpioSET.py
+++ gpioSET.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-PIN = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,14,15,18,23,24,25,8,7,12,16,20,21]
+PIN = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
 
 GPIO.setup(PIN, GPIO.OUT)
 GPIO.output(16, GPIO.LOW)
--- ouijaBoard.py
+++ ouijaBoard.py
@@ -1,21 +1,22 @@
 import time
 import string
 
-LET = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s", "t", "u", "v", "w", "x", "y", "z"]
-ULET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S", "T", "U", "V", "W", "X", "Y", "Z"]
+LET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ULET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 
 def phrase():
+	word = input("Enter phrase: ")
+	wordform = "\n".join(word)
+	for let, ulet in zip(LET, ULET):
+		wordform = wordform.replace(ulet, let)
+		with open("/home/pi/phrase.txt", "w") as phraseFile:
+			phraseFile.write(wordform)
 
-        word = raw_input("Enter phrase: ")
-        wordForm = "\n".join(word)
-        for let, ulet in zip(LET,ULET):
-                wordForm = wordForm.replace(ulet, let)
-                with open("/home/pi/phrase.txt", "w") as phraseFile:
-                        phraseFile.write(wordForm)
 
 while True:
-        try:
-                phrase()
+	try:
+		phrase()
 		quit()
-        except KeyboardInterrupt:
-                quit()
+	except KeyboardInterrupt:
+		quit()
