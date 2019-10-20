755622da7141e0b1ca880cffcbde176aa3a2ec01
Sam O'Keefe <sjo91190@gmail.com>
2019-10-19 19:36:06 -0600

    PEP8 Formatting

--- ouijaBoard.py
+++ ouijaBoard.py
@@ -1,22 +1,24 @@
 import time
 import string
 
-LET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-ULET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+LET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+       "w", "x", "y", "z"]
+ULET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+        "W", "X", "Y", "Z"]
 
 
 def phrase():
-	word = input("Enter phrase: ")
-	wordform = "\n".join(word)
-	for let, ulet in zip(LET, ULET):
-		wordform = wordform.replace(ulet, let)
-		with open("/home/pi/phrase.txt", "w") as phraseFile:
-			phraseFile.write(wordform)
+    word = input("Enter phrase: ")
+    wordform = "\n".join(word)
+    for let, ulet in zip(LET, ULET):
+        wordform = wordform.replace(ulet, let)
+        with open("/home/pi/phrase.txt", "w") as phraseFile:
+            phraseFile.write(wordform)
 
 
 while True:
-	try:
-		phrase()
-		quit()
-	except KeyboardInterrupt:
-		quit()
+    try:
+        phrase()
+        quit()
+    except KeyboardInterrupt:
+        quit()
