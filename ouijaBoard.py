3c6da81b01edbd9d6f5e41e3d25476b36da3c9db
Sam O'Keefe <sjo91190@gmail.com>
2019-10-26 11:41:21 -0600

    Ported to python3

--- ouijaBoard.py
+++ ouijaBoard.py
@@ -1,19 +1,8 @@
-import time
-import string
-
-LET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-       "w", "x", "y", "z"]
-ULET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-        "W", "X", "Y", "Z"]
-
-
 def phrase():
-    word = raw_input("Enter phrase: ")
+    word = input("Enter phrase: ").lower()
     wordform = "\n".join(word)
-    for let, ulet in zip(LET, ULET):
-        wordform = wordform.replace(ulet, let)
-        with open("/home/pi/led-ouija-board/phrase.txt", "w") as phraseFile:
-            phraseFile.write(wordform)
+    with open("/home/pi/led-ouija-board/phrase.txt", "w") as phraseFile:
+        phraseFile.write(wordform)
 
 
 while True:
