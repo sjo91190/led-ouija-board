90c45c6624db23f2a720da0fd326ab648b58e00b
Sam O'Keefe <sjo91190@gmail.com>
2019-10-27 20:12:01 -0600

    Changing to RPi file directory

--- WebGUI/grabphrase.php
+++ WebGUI/grabphrase.php
@@ -1,11 +1,11 @@
 <?php
- $path = 'webphrase.txt';
+ $path = '/home/pi/led-ouija-board/WebGUI/webphrase.txt';
  if (isset($_POST['phrase'])) {
     $fh = fopen($path,"w");
     $string = $_POST['phrase'];
     fwrite($fh,$string);
     fclose($fh);
  }
- header("Location: localhost:8000/landing.html");
+ header("Location: http://ouijaboard");
  exit();
 ?>
